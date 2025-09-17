f2220e5475d9ace5d771c87a4b2b3555277ad419
דנה שטיינקורט <danshe@batyam.org.il>
2025-09-17 15:13:33 +0300

    Added check login function

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 from consts import *
 import socket
 import json
+import ast
+
+
+def check_login(username, password):
+    logged_in = False
+    with open('accounts.txt') as file:
+        for line in file:
+            if line.strip().split(',', 1) == [username, password]:
+                logged_in = True
+    if logged_in:
+        return True
+    else:
+        return False
 
 
 def server_program():
@@ -14,8 +27,14 @@ def server_program():
         data = conn.recv(1024)
         if not data:
             break
-        print(json.loads(data.decode('utf-8')))
-        conn.send(json.dumps({"Response": LOGIN_RESPONSE}).encode())  # send data to the client
+
+        user_data = json.loads(data.decode('utf-8'))["Data"].split(":")
+        print(user_data)
+
+        if check_login(user_data[0], user_data[1]):
+            conn.send(json.dumps({"Response": LOGIN_RESPONSE}).encode())  # send data to the client
+        else:
+            conn.send(json.dumps({"Response": ERROR_CODE}).encode())  # send data to the client
 
     conn.close()  # close the connection
 
